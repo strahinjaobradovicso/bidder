de40bc9cc2901e592a9a437c80c8121dd7f4887e
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-04-13 10:03:54 +0200

    bidding component

--- src/app/components/auction-bidding/auction-bidding.component.ts
+++ src/app/components/auction-bidding/auction-bidding.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BiddingService } from '../../services/bidding.service';
 import { environment } from '../../../environments/environment';
 import { AuctionStatus } from '../../interfaces/model/auctionModel';
@@ -17,7 +17,6 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 export class AuctionBiddingComponent implements OnInit {
 
   auctionKey: string | null;
-  itemId: string | null;
   auctionBid: any;
   auctionStart?: Date;
   auctionStatus = AuctionStatus.Started;
@@ -29,22 +28,73 @@ export class AuctionBiddingComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private biddingService: BiddingService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
   ){
     this.auctionKey = this.route.snapshot.queryParamMap.get(environment.AUCTION_KEY_PARAM);
-    this.itemId = this.route.snapshot.queryParamMap.get(environment.ITEM_ID_PARAM);
+    this.item = this.router.getCurrentNavigation()?.extras.state![environment.ITEM_KEY_STATE];
   }
 
   ngOnInit(): void {
-   
+    if(!this.auctionKey || !this.item){
+      return;
+    }
+    this.biddingService.enterAuctionToServer(this.auctionKey).subscribe({
+      error: (e) => {
+        console.log(e);
+      },
+      complete: () => {
+        console.log('connected');
+      }
+    })
+
+    this.biddingService.enterAuctionToClient().subscribe({
+      next: (v) => {
+        this.auctionBid = v.auctionBid;
+        this.auctionStart = new Date(this.auctionBid.auctionRules.start);
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    })
+
+    this.biddingService.placeBidToClient().subscribe({
+      next: (v) => {
+        this.auctionBid.askValue = v.auctionBid.askValue;
+        this.auctionBid.bidder = v.auctionBid.bidder;
+        this.auctionBid.reachedValue = v.auctionBid.reachedValue;
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    })
+
+    this.biddingService.loweredAskBid().subscribe({
+      next: (v) => {
+        this.auctionBid.askValue = v.askValue;
+      }
+    })
+
+    this.biddingService.auctionResult().subscribe({
+      next: (v) => {
+        console.log(v);
+      }
+    })
   }
 
   bid(){
-
+    this.biddingService.placeBidToServer(this.auctionKey!, this.auctionBid.askValue);
   }
 
   overbid(){
-
+    const input = this.bidForm.value.amount;
+    if(input && input >= this.auctionBid.askValue){
+      this.biddingService.placeBidToServer(this.auctionKey!, input);
+      this.bidForm.reset();
+    }
+    else{
+      console.log('input not valid');
+    }
   }
 }
