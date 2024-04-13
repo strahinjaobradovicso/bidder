8f09d2c938319b6e0b892284adcf2bdad0ad0600
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-04-13 10:04:25 +0200

    auth and bidding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { AuctionListComponent } from './components/auction-list/auction-list.component';
 import { AuctionDetailsComponent } from './components/auction-details/auction-details.component';
 import { AuctionBiddingComponent } from './components/auction-bidding/auction-bidding.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: '', component: AuctionListComponent },
     { path: 'auction-details', component: AuctionDetailsComponent },
-    { path: 'auction-bidding',  component: AuctionBiddingComponent }
+    { path: 'auction-bidding',  component: AuctionBiddingComponent },
+    { path: 'log-in', component: LogInComponent },
+    { path: 'sign-up', component: SignUpComponent }
 ];
