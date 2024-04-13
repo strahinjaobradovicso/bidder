1aeb1513666dff080a30dd2b91ebe0d900390913
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-04-13 09:52:52 +0200

    environment update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,10 @@
 export const environment = {
     API_URL: 'http://localhost:3000',
     AUCTION_KEY_PARAM: 'auction-key',
-    ITEM_ID_PARAM: 'item-id'
+    ITEM_KEY_STATE: 'item-key',
+    AUTH_HEADER_KEY: 'Bearer',
+    TOKEN_STORAGE_KEY: 'token',
+    socket: {
+        BIDDING_NAMESPACE: 'http://localhost:3000/auctions'
+    }
 }
