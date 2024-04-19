0c6dd00a3b81db6d5f92cdbcd77691d62c3a35e8
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-04-19 17:10:29 +0200

    added missing variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
     API_URL: 'http://localhost:3000',
     AUCTION_KEY_PARAM: 'auction-key',
     ITEM_KEY_STATE: 'item-key',
+    AUCTION_KEY_STATE: 'auction-key',
     AUTH_HEADER_KEY: 'Bearer',
     TOKEN_STORAGE_KEY: 'token',
     API_URLS: {
