a7a17acee85d6025909ed474f364874e58880c8c
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-04-24 13:37:43 +0200

    display sign up errors

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -3,26 +3,26 @@ import { AuthComponent } from '../auth/auth.component';
 import { AuthModel } from '../../interfaces/model/authModel';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { Subscription } from 'rxjs';
+import { ErrorComponent } from '../error/error.component';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [AuthComponent],
+  imports: [AuthComponent, ErrorComponent],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-  action = 'signup'
-
-  subscription?: Subscription;
+  action = 'signup';
+  error: Error | null = null;
 
   constructor(private router: Router, private authService: AuthService){}
 
   signup(auth: AuthModel){
+
     this.authService.signup(auth).subscribe({
       error: (e) => {
-        console.log(e);
+        this.error = e;
       },
       complete: () => {
         this.router.navigate(['/log-in']);
