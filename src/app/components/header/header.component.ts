3ba9be34a6c1b89f7828e1c7a42dc448ac3c583e
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-04-25 10:15:40 +0200

    added navbar

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+  naviToggle: boolean = true;
+
+  toggle(){
+    this.naviToggle = !this.naviToggle;
+  }
+}
