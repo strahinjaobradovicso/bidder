0ebe642a32d9fa2a7e3b7cb4651893cbd3400ceb
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-04-25 10:21:57 +0200

    store component router link

--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { ProfileComponent } from '../profile/profile.component';
 import { ItemQueryComponent } from '../item-query/item-query.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-store',
   standalone: true,
-  imports: [ProfileComponent, ItemQueryComponent],
+  imports: [RouterLink, ItemQueryComponent],
   templateUrl: './store.component.html',
   styleUrl: './store.component.css'
 })
