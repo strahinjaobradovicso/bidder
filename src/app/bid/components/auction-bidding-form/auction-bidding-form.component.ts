b70b6aa46880f8c66ce3dcd2fe115f38e728472f
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-07-04 08:44:25 +0200

    bid form effect

--- src/app/bid/components/auction-bidding-form/auction-bidding-form.component.ts
+++ src/app/bid/components/auction-bidding-form/auction-bidding-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, SimpleChanges, input, output } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, effect, input, output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -8,23 +8,23 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './auction-bidding-form.component.html',
   styleUrl: './auction-bidding-form.component.css'
 })
-export class AuctionBiddingFormComponent implements OnChanges {
+export class AuctionBiddingFormComponent {
 
   bid = output<number>();
-
   askingBid = input.required<number>();
-
   overbidForm = new FormGroup({
     overbid: new FormControl(0, [
       Validators.required, Validators.min(0)
     ]),
   });
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.overbidForm.controls['overbid'].setValue(this.askingBid());
-    this.overbidForm.setValidators([Validators.required, Validators.min(this.askingBid())]);
+  
+  constructor() {
+    effect(() => {
+      this.overbidForm.controls['overbid'].setValue(this.askingBid());
+      this.overbidForm.setValidators([Validators.required, Validators.min(this.askingBid())]);
+    })
   }
-
+  
   onOverBid(){
     const overBid = this.overbidForm.value.overbid;
     if(this.overbidForm.valid && overBid){
